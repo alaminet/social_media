68a648b395b0b58cfec02da516e21078e6b36797
Al Amin ET <alaminet.bd@gmail.com>
2024-06-12 21:32:11 +0600

    registration-validation-going-on

--- backend/controllers/userRegistrationController.js
+++ backend/controllers/userRegistrationController.js
@@ -1,13 +1,50 @@
+const { validateEmail, validateName } = require("../helpers/validation");
 const User = require("../model/userModel");
+const bcrypt = require('bcrypt');
 const userRegistrationController = async (req, res) => {
-  const { fname, lname } = req.body;
-
   try {
-    const newUser = await new User({
-      fname: fname,
-      lname: lname,
-    }).save();
-    res.status(200).send({ newUser, message: "Registration Successfull" });
+    const { fname, lname, username, email,password,birthDate } = req.body;
+
+    if (!validateEmail(email)) {
+      return res.status(401).send({ message: "Invalid Email Address" });
+    }
+
+    if (validateName(fname, 3, 7)) {
+      return res
+        .status(401)
+        .send({ message: "First name should be 4 to 6 characters" });
+    }
+    if (validateName(lname, 3, 7)) {
+      return res
+        .status(401)
+        .send({ message: "Last name should be 4 to 6 characters" });
+    }
+    if (validateName(username, 3, 8)) {
+      return res
+        .status(401)
+        .send({ message: "Username should be 4 to 7 characters" });
+    }
+    const usernameExist = await User.findOne({ username: username });
+    const emailExist = await User.findOne({ email: email });
+
+    if (usernameExist) {
+      return res.status(401).send({ message: "Username alread exist" });
+    }
+    if (emailExist) {
+      return res.status(401).send({ message: "Email alread exist" });
+    }
+
+    bcrypt.hash(password, 10, async function(err, hash) {
+        const newUser = await new User({
+            fname: fname,
+            lname: lname,
+            email: email,
+            username: username,
+            password: hash,
+            birthDate: new Date(birthDate)
+          }).save();
+        res.status(200).send({ newUser, message: "Registration Successfull" });
+    });
   } catch (error) {
     res.status(401).send(error);
   }
---
+++ backend/helpers/validation.js
@@ -0,0 +1,15 @@
+exports.validateEmail = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
+
+exports.validateName = (name, min, max) => {
+  if (name > min || name < max) {
+    return true;
+  } else {
+    return false;
+  }
+};
