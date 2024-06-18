a4d93076b89b76547d7e3acb07f8b432ab1cc90c
Al Amin ET <alaminet.bd@gmail.com>
2024-06-18 15:26:35 +0600

    breakpoint-container-setup-done

--- frontend/src/pages/Registration.jsx
+++ frontend/src/pages/Registration.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const Registration = () => {
   return (
-    <div className="text-color-primary font-grilroyRegular">Registration</div>
+    <div className="text-color-primary font-grilroyRegular container">
+      Registration
+    </div>
   );
 };
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -17,7 +17,20 @@ export default {
       grilroyThin: ["Grilroy-Thin"],
       grilroyUltraLight: ["Grilroy-UltraLight"],
     },
-    extend: {},
+    extend: {
+      screens: {
+        xs: "320px",
+        sm: "576px",
+        md: "768px",
+        lg: "992px",
+        xl: "1200px",
+        "2xl": "1400px",
+        "3xl": "1620px",
+      },
+      container: {
+        center: true,
+      },
+    },
   },
   plugins: [],
 };
