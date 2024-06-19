b6aa40cb678e993ffffe5a3a68bd4f0fe2abfd43
Al Amin ET <alaminet.bd@gmail.com>
2024-06-19 11:54:28 +0600

    underage-registration-functional

--- frontend/src/pages/Registration.jsx
+++ frontend/src/pages/Registration.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Formik, useFormik } from "formik";
+import { useFormik } from "formik";
+import { ToastContainer, toast } from "react-toastify";
 import Heading from "../components/Heading";
 import RegistrationSVG from "../assets/svg/RegistrationSVG";
 import Input from "../components/Input";
@@ -23,117 +24,131 @@ const Registration = () => {
     initialValues: initialState,
     validationSchema: signUpValid,
     onSubmit: (values) => {
-      console.log(values);
+      const currentDate = new Date();
+      const pickBDate = new Date(values.birthDate);
+
+      if (currentDate.getFullYear() - pickBDate.getFullYear() < 18) {
+        toast.warn("You are under 18? try again!", {
+          position: "bottom-center",
+          autoClose: 1000,
+          pauseOnHover: false,
+        });
+      } else {
+        console.log(pickBDate.getFullYear() - currentDate.getFullYear());
+      }
     },
   });
   const { errors } = formik;
   return (
-    <div className="relative lg:overflow-hidden">
-      <Helmet>
-        <title>Registration</title>
-      </Helmet>
-      <div className="hidden lg:block absolute w-[350px] h-[350px] bg-color-purple-light rounded-full -left-28 -top-28 -z-10"></div>
-      <div className="hidden lg:block absolute w-[200px] h-[200px] bg-color-purple-light rounded-full -right-28 -top-28 -z-10"></div>
-      <div className="hidden lg:block absolute w-[200px] h-[200px] bg-color-purple-light rounded-full -right-20 -bottom-20 -z-10"></div>
-      <div className="container z-10">
-        <div className="flex justify-center items-center lg:h-screen">
-          <div className="flex gap-4">
-            <div className="hidden md:block">
-              <Heading
-                title="Start Your Journey"
-                className="text-3xl text-center"
-              />
-              <RegistrationSVG className="w-96" />
-            </div>
+    <>
+      <div className="relative lg:overflow-hidden">
+        <ToastContainer />
+        <Helmet>
+          <title>Registration</title>
+        </Helmet>
+        <div className="hidden lg:block absolute w-[350px] h-[350px] bg-color-purple-light rounded-full -left-28 -top-28 -z-10"></div>
+        <div className="hidden lg:block absolute w-[200px] h-[200px] bg-color-purple-light rounded-full -right-28 -top-28 -z-10"></div>
+        <div className="hidden lg:block absolute w-[200px] h-[200px] bg-color-purple-light rounded-full -right-20 -bottom-20 -z-10"></div>
+        <div className="container z-10">
+          <div className="flex justify-center items-center lg:h-screen">
+            <div className="flex gap-4">
+              <div className="hidden md:block">
+                <Heading
+                  title="Start Your Journey"
+                  className="text-3xl text-center"
+                />
+                <RegistrationSVG className="w-96" />
+              </div>
 
-            <div>
-              <div className="sm:w-80 text-color-black">
-                <form onSubmit={formik.handleSubmit}>
-                  <Input
-                    type="text"
-                    placeholder="First Name"
-                    className=""
-                    error={errors.fname}
-                    autocomplete="off"
-                    name="fname"
-                    onChange={formik.handleChange}
-                    value={formik.values.fname}
-                  />
-                  <Input
-                    type="text"
-                    placeholder="Last Name"
-                    className=""
-                    error={errors.lname}
-                    autocomplete="off"
-                    name="lname"
-                    onChange={formik.handleChange}
-                    value={formik.values.lname}
-                  />
-                  <Input
-                    type="email"
-                    placeholder="Email"
-                    className=""
-                    error={errors.email}
-                    autocomplete="off"
-                    name="email"
-                    onChange={formik.handleChange}
-                    value={formik.values.email}
-                  />
-                  <Input
-                    type="password"
-                    placeholder="Password"
-                    className=""
-                    error={errors.password}
-                    autocomplete="current-password"
-                    name="password"
-                    onChange={formik.handleChange}
-                    value={formik.values.password}
-                  />
-                  <div className="flex gap-4 items-center">
-                    <label>Birthday: </label>
+              <div>
+                <div className="sm:w-80 text-color-black">
+                  <form onSubmit={formik.handleSubmit}>
                     <Input
-                      className="w-full"
-                      type="date"
+                      type="text"
+                      placeholder="First Name"
+                      className=""
+                      error={errors.fname}
                       autocomplete="off"
-                      error={errors.birthDate}
-                      name="birthDate"
+                      name="fname"
                       onChange={formik.handleChange}
-                      value={formik.values.birthDate}
+                      value={formik.values.fname}
                     />
-                  </div>
-                  <div className="flex gap-4">
-                    <InputRadio
-                      name="gender"
-                      value="Male"
+                    <Input
+                      type="text"
+                      placeholder="Last Name"
+                      className=""
+                      error={errors.lname}
                       autocomplete="off"
+                      name="lname"
                       onChange={formik.handleChange}
+                      value={formik.values.lname}
                     />
-                    <InputRadio
-                      name="gender"
-                      value="Female"
+                    <Input
+                      type="email"
+                      placeholder="Email"
+                      className=""
+                      error={errors.email}
                       autocomplete="off"
+                      name="email"
                       onChange={formik.handleChange}
+                      value={formik.values.email}
                     />
-                    <InputRadio
-                      name="gender"
-                      value="Others"
-                      autocomplete="off"
+                    <Input
+                      type="password"
+                      placeholder="Password"
+                      className=""
+                      error={errors.password}
+                      autocomplete="current-password"
+                      name="password"
                       onChange={formik.handleChange}
+                      value={formik.values.password}
                     />
-                  </div>
-                  <Button className="mt-6 mb-2 w-full" type="submit">
-                    Submit
-                  </Button>
-                  <Link className="text-color-primary" to="#">
-                    Already have an account !
-                  </Link>
-                </form>
+                    <div className="flex gap-4 items-center">
+                      <label>Birthday: </label>
+                      <Input
+                        className="w-full"
+                        type="date"
+                        autocomplete="off"
+                        error={errors.birthDate}
+                        name="birthDate"
+                        onChange={formik.handleChange}
+                        value={formik.values.birthDate}
+                      />
+                    </div>
+                    <div className="flex gap-4">
+                      <InputRadio
+                        name="gender"
+                        value="Male"
+                        autocomplete="off"
+                        onChange={formik.handleChange}
+                      />
+                      <InputRadio
+                        name="gender"
+                        value="Female"
+                        autocomplete="off"
+                        onChange={formik.handleChange}
+                      />
+                      <InputRadio
+                        name="gender"
+                        value="Others"
+                        autocomplete="off"
+                        onChange={formik.handleChange}
+                      />
+                    </div>
+                    <Button className="mt-6 mb-2 w-full" type="submit">
+                      Submit
+                    </Button>
+                    <Link className="text-color-primary" to="#">
+                      Already have an account !
+                    </Link>
+                  </form>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
