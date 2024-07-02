be309a587669c57ae8eeb01c9e3caeaff9ef2116
Al Amin ET <alaminet.bd@gmail.com>
2024-07-02 07:05:05 +0600

    xs-responsive-done

--- frontend/src/components/SearchBox.jsx
+++ frontend/src/components/SearchBox.jsx
@@ -6,22 +6,27 @@ const SearchBox = () => {
   const [show, setShow] = useState(false);
   const clickOutSide = useRef(null);
   useClickOutside(clickOutSide, () => setShow(false));
+  const handleClick = () => {
+    setShow(true);
+    document.getElementById("inputfiled").focus();
+  };
 
   return (
     <>
-      <div className="absolute right-10 lg:relative">
-        <form className="" action="">
-          <div className="relative" onClick={() => setShow(true)}>
+      <div className={`${show && "absolute right-2 z-10 lg:relative"}`}>
+        <form action="">
+          <div className="relative">
             {!show && (
-              <div className="absolute top-2 left-2">
+              <div onClick={handleClick} className="absolute top-2 left-2">
                 <SearchIcon />
               </div>
             )}
             <div>
               <input
+                id="inputfiled"
                 onClick={() => setShow(true)}
-                className={`outline-none border rounded-full lg:py-2 lg:pl-10 lg:pr-2 w-10 h-10 lg:w-80 transition-all ease-linear duration-150 ${
-                  show && "lg:pl-4 lg:border-0 shadow-lg"
+                className={`outline-none border rounded-full p-4 lg:py-2 lg:pl-10 lg:pr-2 w-10 h-10 lg:w-80 transition-all ease-linear duration-150 ${
+                  show && "w-60 lg:pl-4 lg:border-0 shadow-lg"
                 }`}
                 type="text"
                 placeholder="Search..."
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,46 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import SidebarLeft from "../components/SidebarLeft";
 import SearchBox from "../components/SearchBox";
 import SidebarRight from "../components/SidebarRight";
+import defaultProPic from "../assets/images/avatar_boy_cap.png";
+import { Messages } from "../assets/svg/Messages";
+import { Friends } from "../assets/svg/Friends";
+import { NewsFeed } from "../assets/svg/NewsFeed";
+import { Media } from "../assets/svg/Media";
+import { Settings } from "../assets/svg/Settings";
 
 const Home = () => {
   const { userInfo } = useSelector((user) => user.loginSlice);
   const [state, setState] = useState();
+
+  const menuData = [
+    {
+      title: "News Feed",
+      icon: NewsFeed,
+      key: "newsfeed",
+    },
+    {
+      title: "Message",
+      icon: Messages,
+      key: "message",
+    },
+    {
+      title: "Friends",
+      icon: Friends,
+      key: "friends",
+    },
+    {
+      title: "Media",
+      icon: Media,
+      key: "media",
+    },
+    {
+      title: "Settings",
+      icon: Settings,
+    },
+  ];
 
   return (
     <>
@@ -18,9 +51,26 @@ const Home = () => {
           </div>
           <div>
             <div className="py-5">
-              <div className="lg:flex lg:gap-4 lg:justify-between">
+              <div className="flex gap-4 justify-between">
                 <div className="hidden lg:block capitalize font-grilroyMedium">
                   {state}
+                </div>
+                <div className="lg:hidden">
+                  <div className="w-10 h-10 rounded-full bg-color-purple-light mx-auto mb-2 overflow-hidden object-cover">
+                    <img
+                      src={userInfo.profilePic || defaultProPic}
+                      alt={userInfo.username || "Profile Picture"}
+                    />
+                  </div>
+                </div>
+                <div className="lg:hidden flex gap-1 justify-center items-center">
+                  {menuData?.map((item, i) => (
+                    <Link to={item.key}>
+                      <div className="flex justify-center items-center w-8 h-8 rounded-full hover:bg-color-blue hover:text-color-white text-sm">
+                        <item.icon />
+                      </div>
+                    </Link>
+                  ))}
                 </div>
                 <div>
                   <SearchBox />
